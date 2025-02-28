7caf1de7536323c8c98db826a3d9c41d6f53253e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-27 19:45:39 -0500

    Update i2c_shtc3.h

--- i2c_shtc3.h
+++ i2c_shtc3.h
@@ -2,26 +2,27 @@
 #define i2c_shtc3_h
 
 #include <Wire.h>
-#include <Adafruit_SHTC3.h>
+#include <SparkFun_SHTC3.h>
 
 #include "NmeaXDR.h"
 #include "Nmea0183Msg.h"
 
 #define SHTC3_I2C_ADDR 0x70  // SHTC3 I2C address (0x71 is 'read' address)
 
-Adafruit_SHTC3 i2c_shtc3_sensor;  
+SHTC3 i2c_shtc3_sensor;  
 
 void i2c_shtc3_report() {
-  sensors_event_t humidity, temp;
-  i2c_shtc3_sensor.getEvent(&humidity, &temp);
-  gen_nmea0183_xdr("$BBXDR,H,%.2f,P,HUMI_SHTC3", humidity.relative_humidity);  // %
-  gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_SHTC3", temp.temperature);            // C
+  SHTC3_Status_TypeDef result = i2c_shtc3_sensor.update();
+  if (result == SHTC3_Status_Nominal) {
+    gen_nmea0183_xdr("$BBXDR,H,%.2f,P,HUMI_SHTC3", i2c_shtc3_sensor.toPercent());  // %
+    gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_SHTC3", i2c_shtc3_sensor.toDegC());     // C
+  }
 }
 
 bool i2c_shtc3_try_init() {
   bool i2c_shtc3_found = false;
   for (int i = 0; i < 3; i++) {
-    i2c_shtc3_found = i2c_shtc3_sensor.begin(&Wire);  // Initialize SHTC3 sensor
+    i2c_shtc3_found = i2c_shtc3_sensor.begin(Wire);  // Initialize SHTC3 sensor
     if (i2c_shtc3_found) {
       break;
     }
