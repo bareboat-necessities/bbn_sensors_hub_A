707f538352de97c105a9b4dac17b7633d9cf51a5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-01 09:33:42 -0500

    Update gpio_sensors.h

--- gpio_sensors.h
+++ gpio_sensors.h
@@ -1,9 +1,12 @@
 #ifndef gpio_sensors_h
 #define gpio_sensors_h
 
+#include "gpio_pir_as312.h"
+#include "gpio_limit_switch.h"
 
 void gpio_sensors_init() {
-  gpio_jsn_sr04t_try_init();
+  gpio_pir_as312_try_init();
+  gpio_limit_switch_try_init();
 }
 
 #endif
