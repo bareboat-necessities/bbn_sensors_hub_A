08bc26048e85e72c612f4dafe650baf14a1c6e22
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-31 22:27:25 -0400

    Update i2c_as3935.h

--- i2c_as3935.h
+++ i2c_as3935.h
@@ -34,24 +34,25 @@ void AS3935_ISR();
 DFRobot_AS3935_I2C  i2c_as3935_sensor((uint8_t)AS3935_IRQ_PIN, (uint8_t)AS3935_I2C_ADDR);
 
 void i2c_as3935_report() 
-  if (digitalRead(lightningInt) == HIGH) {
-    // Hardware has alerted us to an event, now we read the interrupt register
-    // to see exactly what it is.
-    uint8_t intVal = i2c_as3935_sensor.readInterruptReg();
-    if (intVal == NOISE_INT) {
-      // Noise Detected!
-      //Serial.println("Noise Detected!");
-    }
-    else if (intVal == DISTURBER_INT) {
-      // Disturber Detected!
-      //Serial.println("Disturber Detected!");
-    }
-    else if (intVal == LIGHTNING_INT) {
-      //Serial.println("Lightning Strike Detected!");
+  if (AS3935IsrTrig == 1) {
+    delay(5);
+    AS3935IsrTrig = 0;
+    uint8_t intSrc = i2c_as3935_sensor.getInterruptSrc();
+    if (intSrc == 1) {
+      // Get distance data
+      uint8_t lightningDistKm = i2c_as3935_sensor.getLightningDistKm();
+
+      // Get lightning energy intensity
+      uint32_t lightningEnergyVal = i2c_as3935_sensor.getStrikeEnergyRaw();
+
       // Lightning! Now how far away is it? Distance estimation takes into
       // account any previously seen events in the last 15 seconds.
-      gen_nmea0183_xdr("$BBXDR,D,%.0f,M,LIGHTNING_RANGE", (float)i2c_as3935_sensor.distanceToStorm() * 1000.0);
-      gen_nmea0183_xdr("$BBXDR,X,%.0f,,LIGHTNING_LEVEL", (float)i2c_as3935_sensor.lightningEnergy());
+      gen_nmea0183_xdr("$BBXDR,D,%.0f,M,LIGHTNING_RANGE", (float)lightningDistKm * 1000.0);
+      gen_nmea0183_xdr("$BBXDR,X,%.0f,,LIGHTNING_LEVEL", (float)lightningEnergyVal);
+    } else if (intSrc == 2){
+      //Serial.println("Disturber discovered!");
+    } else if (intSrc == 3){
+      //Serial.println("Noise level too high!");
     }
   }
 }
