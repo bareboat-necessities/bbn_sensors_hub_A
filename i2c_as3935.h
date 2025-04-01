469d60605b58b6aa0e7ed767d1620d967fd0016d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-04-01 18:02:01 -0400

    Update i2c_as3935.h

--- i2c_as3935.h
+++ i2c_as3935.h
@@ -85,7 +85,7 @@ void i2c_as3935_report() {
   }
   if (noise_adj_last_ != 0L && millis() - noise_adj_last_ > NOISE_ADJ_INTERVAL) {
     uint8_t nf_lev = readNoiseFloorThreshold();
-    if (nf_lev > AS3935_NFL_1) {
+    if (nf_lev > AS3935MI::AS3935_NFL_1) {
       i2c_as3935_sensor.decreaseNoiseFloorThreshold();
       noise_adj_last_ = millis();
     }
