45c3813c4ab134ec69c30e115c6ee2394c6c068a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-31 23:27:44 -0400

    Update i2c_as3935.h

--- i2c_as3935.h
+++ i2c_as3935.h
@@ -35,7 +35,7 @@ DFRobot_AS3935_I2C i2c_as3935_sensor((uint8_t)AS3935_IRQ_PIN, (uint8_t)AS3935_I2
 
 void i2c_as3935_report() {
   if (AS3935IsrTrig == 1) {
-    delay(3);
+    delay(5);
     AS3935IsrTrig = 0;
     uint8_t intSrc = i2c_as3935_sensor.getInterruptSrc();
     if (intSrc == 1) {
@@ -50,9 +50,9 @@ void i2c_as3935_report() {
       gen_nmea0183_xdr("$BBXDR,D,%.0f,M,LIGHTNING_RANGE", (float)lightningDistKm * 1000.0);
       gen_nmea0183_xdr("$BBXDR,X,%.0f,,LIGHTNING_LEVEL", (float)lightningEnergyVal);
     } else if (intSrc == 2) {
-      //Serial.println("Disturber discovered!");
+      gen_nmea0183_msg("$BBTXT,01,01,01,ENVIRONMENT LIGHTNING %s", "Disturber discovered");
     } else if (intSrc == 3) {
-      //Serial.println("Noise level too high!");
+      gen_nmea0183_msg("$BBTXT,01,01,01,ENVIRONMENT LIGHTNING %s", "Noise level too high");
     }
   }
 }
@@ -64,7 +64,7 @@ bool i2c_as3935_try_init() {
     attachInterrupt(digitalPinToInterrupt(AS3935_IRQ_PIN), AS3935_ISR, RISING);
     i2c_as3935_sensor.manualCal(AS3935_CAPACITANCE, AS3935_MODE, AS3935_DIST);
     gen_nmea0183_msg("$BBTXT,01,01,01,ENVIRONMENT found as3935 sensor at address=0x%s", String(AS3935_I2C_ADDR, HEX).c_str());
-    app.onRepeat(5, []() {
+    app.onRepeat(10, []() {
       i2c_as3935_report();
     });
   }
